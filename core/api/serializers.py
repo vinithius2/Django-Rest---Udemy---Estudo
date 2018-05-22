4260ebb666d9d5aa394042b952093baab183e603
vinithius2 <marcos.vinithius@gmail.com>
2018-05-22 19:20:03 -0300

    Otimizando Serializer

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,8 +1,27 @@
+from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
+from atracoes.api.serializers import AtracoesSerializer
+from avaliacoes.api.serializers import AvaliacaoSerializer
+from comentarios.api.serializers import ComentarioSerializer
 from core.models import PontoTuristico
+from enderecos.api.serializers import EnderecoSerializer
 
 
 class PontoTuristicoSerializer(ModelSerializer):
+    atracoes = AtracoesSerializer(many=True)
+    endereco = EnderecoSerializer()
+    comentarios = ComentarioSerializer(many=True)
+    avaliacoes = AvaliacaoSerializer(many=True)
+    descricao_completa_01 = SerializerMethodField()
+
     class Meta:
         model = PontoTuristico
-        fields = ('id', 'nome', 'descricao', 'aprovado', 'foto')
+        fields = (
+            'id', 'nome', 'descricao', 'aprovado',
+            'atracoes', 'comentarios', 'avaliacoes',
+            'endereco', 'foto', 'descricao_completa_01',
+            'descricao_completa_02',
+        )
+
+    def get_descricao_completa_01(self, obj):
+        return '{0} - {1}'.format(obj.nome, obj.descricao)
--- core/models.py
+++ core/models.py
@@ -15,5 +15,9 @@ class PontoTuristico(models.Model):
     endereco = models.ForeignKey(Endereco, null=True, blank=True, on_delete=models.CASCADE)
     foto = models.ImageField(upload_to='pontos_turisticos', null=True, blank=True)
 
+    @property
+    def descricao_completa_02(self):
+        return '{0} - {1}'.format(self.nome, self.descricao)
+
     def __str__(self):
         return self.nome
